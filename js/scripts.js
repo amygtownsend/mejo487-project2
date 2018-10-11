2d624e4394bd2dcc9d31da2a5ad8fd29bffd916c
Amy Townsend <amygtownsend@gmail.com>
2018-10-11 00:22:11 -0400

    first commit set up slider plugin

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var myFullpage = new fullpage('#fullpage', {
+  anchors:['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8'],
+  sectionsColor: ['#4CCFB3', '#4CCFB3', '#4CCFB3', '#4CCFB3', '#4CCFB3', '#4CCFB3', '#4CCFB3', '#4CCFB3'],
+  navigationTooltips: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8'],
+  css3: true,
+  scrollingSpeed: 1000,
+  navigation: true,
+  responsiveHeight: 330,
+  controlArrows: false,
+  showActiveTooltip: true,
+  menu: '#menu'
+});
