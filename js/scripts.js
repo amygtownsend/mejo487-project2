514fe7448547313968579432555e3c715750ebce
Amy Townsend <amygtownsend@gmail.com>
2018-10-11 00:35:16 -0400

    faster scrolling speed, added SEO meta meta tags

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ var myFullpage = new fullpage('#fullpage', {
   sectionsColor: ['#4CCFB3', '#4CCFB3', '#4CCFB3', '#4CCFB3', '#4CCFB3', '#4CCFB3', '#4CCFB3', '#4CCFB3'],
   navigationTooltips: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8'],
   css3: true,
-  scrollingSpeed: 1000,
+  scrollingSpeed: 800,
   navigation: true,
   responsiveHeight: 330,
   controlArrows: false,
