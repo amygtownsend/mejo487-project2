d19eb02db7a8b7c60734b307b1e7bd723c93d2db
Amy Townsend <amygtownsend@gmail.com>
2018-10-15 08:02:43 -0400

    set up quiz

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,72 @@ var myFullpage = new fullpage('#fullpage', {
   showActiveTooltip: true,
   menu: '#menu'
 });
+
+$( function() {
+   $( "#draggable1" ).draggable({revert: "invalid"});
+   $( "#draggable2" ).draggable({revert: "invalid"});
+   $( "#draggable3" ).draggable({revert: "invalid"});
+   $( "#draggable4" ).draggable({revert: "invalid"});
+   $( "#droppable1" ).droppable({
+     drop: function( event, ui ) {
+       $( this )
+         .addClass( "ui-state-highlight" )
+         .find( "p" )
+           .html( "Dropped!" );
+     }
+   });
+   $( "#droppable2" ).droppable({
+     drop: function( event, ui ) {
+       $( this )
+         .addClass( "ui-state-highlight" )
+         .find( "p" )
+           .html( "Dropped!" );
+     }
+   });
+   $( "#droppable3" ).droppable({
+     drop: function( event, ui ) {
+       $( this )
+         .addClass( "ui-state-highlight" )
+         .find( "p" )
+           .html( "Dropped!" );
+     }
+   });
+   $( "#droppable4" ).droppable({
+     drop: function( event, ui ) {
+       $( this )
+         .addClass( "ui-state-highlight" )
+         .find( "p" )
+           .html( "Dropped!" );
+     }
+   });
+ } );
+
+// $( function() {
+//
+//     // There's the gallery and the trash
+//     var $tasks = $( "#tasks" ),
+//       $one = $( "#one" ),
+//       $two = $( "#two" ),
+//       $three = $( "#three" ),
+//       $four = $( "#four" ),;
+//
+//     // Let the gallery items be draggable
+//     $( "li", $tasks ).draggable({
+//       revert: "invalid", // when not dropped, the item will revert back to its initial position
+//     });
+//
+//     // Let the trash be droppable, accepting the gallery items
+//     $one.droppable({
+//       accept: "#tasks > li",
+//       classes: {
+//         "ui-droppable-active": "ui-state-active",
+//         "ui-droppable-hover": "ui-state-hover"
+//       },
+//       drop: function( event, ui ) {
+//         $( this )
+//           .addClass( "ui-state-highlight" )
+//           .find( "p" )
+//             .html( "Dropped!" );
+//       }
+//     });
+//   } );
