5b43d56f72f8b0df97484869956d5353b4c77b6a
Amy Townsend <amygtownsend@gmail.com>
2018-10-15 12:08:29 -0400

    add slide content, add first quiz question

--- js/slickQuiz-config.js
+++ js/slickQuiz-config.js
@@ -4,8 +4,8 @@
 
 var quizJSON = {
     "info": {
-        "name":    "Test Your Knowledge!!",
-        "main":    "<p>Think you're smart enough to be on Jeopardy? Find out with this super crazy knowledge quiz!</p>",
+        "name":    "Test Your Knowledge!",
+        "main":    "<p>Let's see how much you learned about productivity techniques and principles. Take the quiz!</p>",
         "results": "<h5>Learn More</h5><p>Etiam scelerisque, nunc ac egestas consequat, odio nibh euismod nulla, eget auctor orci nibh vel nisi. Aliquam erat volutpat. Mauris vel neque sit amet nunc gravida congue sed sit amet purus.</p>",
         "level1":  "Jeopardy Ready",
         "level2":  "Jeopardy Contender",
@@ -15,15 +15,15 @@ var quizJSON = {
     },
     "questions": [
         { // Question 1 - Multiple Choice, Single True Answer
-            "q": "What number is the letter A in the English alphabet?",
+            "q": "According to the Eisenhower Method, you should schedule tasks to complete later if they are...",
             "a": [
-                {"option": "8",      "correct": false},
-                {"option": "14",     "correct": false},
-                {"option": "1",      "correct": true},
-                {"option": "23",     "correct": false} // no comma here
+                {"option": "Important / Urgent",      "correct": false},
+                {"option": "Important / Not Urgent",     "correct": true},
+                {"option": "Not Important / Urgent",      "correct": false},
+                {"option": "Not Important / Not Urgent",     "correct": false} // no comma here
             ],
-            "correct": "<p><span>That's right!</span> The letter A is the first letter in the alphabet!</p>",
-            "incorrect": "<p><span>Uhh no.</span> It's the first letter of the alphabet. Did you actually <em>go</em> to kindergarden?</p>" // no comma here
+            "correct": "<p><span>That's right!</span> Schedule tasks that are important but not urgent!</p>",
+            "incorrect": "<p><span>Sorry.</span> It's actually tasks that are important but not urgent that you should schedule</p>" // no comma here
         },
         { // Question 2 - Multiple Choice, Multiple True Answers, Select Any
             "q": "Which of the following best represents your preferred breakfast?",
